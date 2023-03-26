f0649c9cdebd498187c5beeede1a00a0b550dd6e
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-26 02:19:55 +0000

    adds jotai impl

--- jotai-CRUD-practice-todo/src/App.tsx
+++ jotai-CRUD-practice-todo/src/App.tsx
@@ -1,5 +1,63 @@
+import React, { FormEvent } from 'react';
+import { atom, useAtom } from 'jotai';
+
+type Todo = {
+  id: number | null,
+  title: string,
+}
+
+type Todos = Todo[];
+
+const todoAtom = atom<Todo>({ id: null, title: '' });
+const todoListAtom = atom<Todos>([]);
+
+const TodoComponent = ({ todoItem }: { todoItem: Todo }) => {
+  return (
+    <>
+      <p>{todoItem.title}</p>
+    </>
+  )
+}
+
+const TodoList = () => {
+  const [todo, setTodo] = useAtom(todoAtom)
+  const [todos, setTodos] = useAtom(todoListAtom);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    setTodos((prev) => [...prev, todo]);
+
+    setTodo({ id: null, title: '' });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>To Do</h1>
+        <input
+          type="text"
+          placeholder='Enter a todo item'
+          value={todo.title} onChange={(e) => setTodo({ id: Math.floor(Math.random() * 10000) + 1, title: e.target.value })}
+        />
+        <button type='submit'>Add</button>
+      </form>
+
+      {todos.map(todoItem => {
+        return <React.Fragment key={todoItem.id}>
+          <TodoComponent todoItem={todoItem} />
+        </React.Fragment>
+      })}
+    </>
+  )
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
